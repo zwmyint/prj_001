834a794c7436ac3d6abca8d026ea041fe6c6229a
ZAW WIN MYINT <45326766+zwmyint@users.noreply.github.com>
2020-12-21 16:07:42 +0800

    2nd commit

--- ASPNetCore5WEBAPI/Controllers/ProductsController.cs
+++ ASPNetCore5WEBAPI/Controllers/ProductsController.cs
@@ -1,11 +1,56 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace ASPNetCore5WEBAPI.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
+        /* [HttpGet]
+        public string Get()
+        {
+            https://localhost:5001/api/products
+            return "Lots of products";
+        } */
+        
+       /*  [HttpGet("{id}")]
+        public string GetById(int id, [FromQuery] bool isActive)
+        {
+            //https://localhost:5001/api/products/10
+            //https://localhost:5001/api/products/10?isActive=True
+            return $"Lots of products: {id}, status: {isActive}";
+        } */
 
+        [HttpGet]
+        public string GetByObject([FromQuery] ProductDTO productDTO)
+        {
+            //https://localhost:5001/api/products
+            //https://localhost:5001/api/products?Id=20&name=myproduct
+            return $"product id: {productDTO.Id}, name: {productDTO.Name}";
+        }
+
+        [HttpPost]
+        public IActionResult Post(ProductDTO productDTO)
+        {
+            //https://localhost:5001/api/products
+            return Ok($"product id: {productDTO.Id}, name: {productDTO.Name}");
+
+            /* {
+                "Id": 2001,
+                "Name": "ZAW"
+            }
+            */
+        }
 
         //
     }
+
+    public class ProductDTO
+    {
+        public int Id {get; set;}
+        public string Name {get; set;}
+    }
+
+    //
+
 }
